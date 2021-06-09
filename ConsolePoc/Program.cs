3d6ec2c3798f89acb2918673992da1d101df2191
Denton <matthew.denton@billgo.com>
2021-06-09 05:55:01 -0600

    Add project files.

---
+++ ConsolePoc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
