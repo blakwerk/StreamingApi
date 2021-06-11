66dca7a89e1b4296c57205dcec69582cd450e385
Denton <matthew.denton@billgo.com>
2021-06-11 08:31:27 -0600

    Add hardcoded streaming endpoint client with tweets dumped to logs

--- Streaming.Api.Implementation/Services/TweetStreamConnectionService.cs
+++ Streaming.Api.Implementation/Services/TweetStreamConnectionService.cs
@@ -6,9 +6,9 @@
     using Microsoft.Extensions.Logging;
     using RestSharp;
     using RestSharp.Authenticators;
-    using Streaming.Api.Core.Configuration;
     using Streaming.Api.Core.Services;
-    using Streaming.Api.Implementation.Configuration;
+    using Tweetinvi;
+    using Tweetinvi.Events;
 
     internal class TweetStreamConnectionService : ITweetStreamConnection
     {
@@ -29,6 +29,78 @@
         }
 
         public async Task ConnectToSampledStreamAsync()
+        {
+            _logger.LogInformation("Connecting to stream.");
+
+            try
+            {
+                var bearerToken = "AAAAAAAAAAAAAAAAAAAAAN0CQgEAAAAAPtniRfuF%2BBWhLQ6DpQvltGc5En0%3DkWZQgqeNCXRWiPsQhVMdhM7gt8ydq7tQAvZfgCZNTz7UdfHxm6";
+
+                var consumerKey = "PyYoQXHCrJyap2kun19yHAHjO";
+                var consumerSecretKey = "jFRZFvS8LyCVbiJTNESLujnKfGJTrjEiDvnt2rb09v0sGAHiNX";
+
+                var client = new TwitterClient(consumerKey, consumerSecretKey, bearerToken);
+                //var authRequest = await client.Auth.RequestAuthenticationUrlAsync();
+
+                var currentSampleCount = 0;
+                var sampleStopCount = 10000;
+
+                this._logger.LogInformation("Beginning stream.");
+                var sampleStreamV2 = client.StreamsV2.CreateSampleStream();
+                sampleStreamV2.EventReceived += SampleStreamV2_EventReceived;
+
+                sampleStreamV2.TweetReceived += async (sender, args) =>
+                {
+                    try
+                    {
+                        this._logger.LogDebug(args.Tweet.Text);
+                        
+                        //TODO map streaming tweets to "incoming API objects" and send to our API (or queue them)
+
+                        currentSampleCount++;
+                        if (currentSampleCount >= sampleStopCount)
+                        {
+                            sampleStreamV2.StopStream();
+                            this._logger.LogInformation("Completing stream.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        this._logger.LogError(ex, "An exception occurred.");
+                        // for the purposes of this stream reader, skip forward if we're hitting exceptions 
+                        currentSampleCount += 10;
+                    }
+                };
+
+                //var parameters = new StartSampleStreamV2Parameters{}
+
+                await sampleStreamV2.StartAsync();
+
+                //this._logger.LogInformation(response.Content);
+
+                //if (response.IsSuccessful)
+                //{
+                //    _logger.LogInformation("Stream connected.");
+                //}
+                //else
+                //{
+                //    _logger.LogError("Unable to connect to stream! " +
+                //                     $"Response: {(int)response.StatusCode} {response.StatusCode}");
+                //}
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception thrown when connecting to stream");
+                throw;
+            }
+        }
+
+        private void SampleStreamV2_EventReceived(object sender, StreamEventReceivedArgs e)
+        {
+            this._logger.LogInformation($"Twitter stream event received: {e.Json}");
+        }
+
+        public async Task ConnectToSampledStreamAsync_RestClient()
         {
             _logger.LogInformation("Connecting to stream.");
 
